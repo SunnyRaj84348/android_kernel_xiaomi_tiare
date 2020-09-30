6f2bf2e7642665f1a1c052a90a8c25b71599c49c
SunnyRaj84348 <sunnyraj7461@gmail.com>
2020-09-30 15:05:12 +0700

    drivers: of: Import batterydata changes
    Signed-off-by: SunnyRaj84348 <sunnyraj7461@gmail.com>

--- drivers/of/of_batterydata.c
+++ drivers/of/of_batterydata.c
@@ -310,32 +310,26 @@ static int64_t of_batterydata_convert_battery_id_kohm(int batt_id_uv,
 	return resistor_value_kohm;
 }
 
+int battery_type_id = 0 ;
+
+static char *default_batt_type = "Generic_Battery";
+
+extern int cw_get_battid_for_profile_check(void);
+
 struct device_node *of_batterydata_get_best_profile(
 		const struct device_node *batterydata_container_node,
 		const char *psy_name,  const char  *batt_type)
 {
 	struct batt_ids batt_ids;
 	struct device_node *node, *best_node = NULL;
-	struct power_supply *psy;
 	const char *battery_type = NULL;
-	union power_supply_propval ret = {0, };
 	int delta = 0, best_delta = 0, best_id_kohm = 0, id_range_pct,
 		batt_id_kohm = 0, i = 0, rc = 0, limit = 0;
 	bool in_range = false;
+	bool default_id = false;
 
-	psy = power_supply_get_by_name(psy_name);
-	if (!psy) {
-		pr_err("%s supply not found. defer\n", psy_name);
-		return ERR_PTR(-EPROBE_DEFER);
-	}
-
-	rc = psy->get_property(psy, POWER_SUPPLY_PROP_RESISTANCE_ID, &ret);
-	if (rc) {
-		pr_err("failed to retrieve resistance value rc=%d\n", rc);
-		return ERR_PTR(-ENOSYS);
-	}
-
-	batt_id_kohm = ret.intval / 1000;
+	batt_id_kohm = cw_get_battid_for_profile_check();
+	pr_err("C3G batt_id_kohm = %d\n", batt_id_kohm);
 
 	/* read battery id range percentage for best profile */
 	rc = of_property_read_u32(batterydata_container_node,
@@ -388,13 +382,29 @@ struct device_node *of_batterydata_get_best_profile(
 	}
 
 	if (best_node == NULL) {
+		for_each_child_of_node(batterydata_container_node, node) {
+			if (default_batt_type != NULL) {
+				rc = of_property_read_string(node, "qcom,battery-type",
+								&battery_type);
+				if (!rc && strcmp(battery_type, default_batt_type) == 0) {
+					best_node = node;
+					best_id_kohm = batt_id_kohm;
+					default_id = true;
+					pr_err("No battery data found, Use default battery data\n");
+					break;
+				}
+			}
+		}
+	}
+
+	if (best_node == NULL) {
 		pr_err("No battery data found\n");
 		return best_node;
 	}
 
 	/* check that profile id is in range of the measured batt_id */
 	if (abs(best_id_kohm - batt_id_kohm) >
-			((best_id_kohm * id_range_pct) / 100)) {
+			((best_id_kohm * id_range_pct) / 100)&&!default_id) {
 		pr_err("out of range: profile id %d batt id %d pct %d",
 			best_id_kohm, batt_id_kohm, id_range_pct);
 		return NULL;
